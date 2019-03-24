d969352f8c927b257972344e65d159c8a1d00364
Nikolay Tarkaiev <30959733+nikolay-tarkaev@users.noreply.github.com>
2019-03-24 20:49:29 +0200

    Update 404_view.php

--- app/views/404_view.php
+++ app/views/404_view.php
@@ -1,10 +1,3 @@
-<style>
-	.img_404 {
-		padding: 0;
-		position: absolute;
-		top: 50%;
-		left: 50%;
-		margin: -217px 0 0 -257px;
-	}
-</style>
-<img class="img_404" src="/images/404.png" />
+<div class="text-center">
+	<img src="/images/404.png" alt="Page not found" title="Page not found" />
+</div>
